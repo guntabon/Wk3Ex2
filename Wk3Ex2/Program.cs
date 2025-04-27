6822dd103efa91d1e7d6bafd831705412869935e
Gunner Flory <g0364875@amarillcollege.com>
2025-04-27 15:55:44 -0500

    Commit 3

--- Wk3Ex2/Program.cs
+++ Wk3Ex2/Program.cs
@@ -10,90 +10,65 @@ namespace Wk3Ex2
     {
         static void Main(string[] args)
         {
-            bool myWhile = true;
+            double firstNum = 0.0;
 
+            double secondNum = 0.0;
 
-            //This line declares secondNum as a double variable and allows it for future use
-            double secondNum = 0;
-
-            //Creates myChoice as an int variable
             int myChoice = 0;
 
+            double userOutPut = 0;
 
-            while (myWhile){
-                //Presents the name of the program an its use
-            Console.WriteLine("CALCULATOR APPLICATION");
+            bool whileTrue = true;
 
-                //Prompts the user to input the first number to be used in the application
-            Console.WriteLine("What is the first number?");
+            while (whileTrue) {
+                Console.WriteLine("CALCULATOR APPLICATION");
 
+                Console.WriteLine("What is the first number?");
 
-                //Prompts user to enter the second number. 
-            Console.WriteLine("What is the second number?");
+                firstNum = Convert.ToDouble(Console.ReadLine());
 
+                Console.WriteLine("What is the second number?");
 
+                secondNum = Convert.ToDouble(Console.ReadLine());
 
-                //Presents the options for use in the application.
-            Console.WriteLine("Select an operation: ");
+                Console.WriteLine("Select an operation: ");
+                Console.WriteLine("1 - Addition");
+                Console.WriteLine("2 - Subtraction");
+                Console.WriteLine("3 - Multiplication");
+                Console.WriteLine("4 - Division");
+                Console.WriteLine("5 - Quit");
 
-                //The first option is adding the two numbers together
-            Console.WriteLine("1 - Addition");
+                Console.WriteLine("Enter your choice (1 - 4): ");
 
-                //The second allows users to subtract
-            Console.WriteLine("2 - Subtraction");
+                myChoice = Convert.ToInt32(Console.ReadLine());
 
-                //The third allows users to multiply the two numbers
-            Console.WriteLine("3 - Multiplication");
-
-                //This prompts the user to select division
-            Console.WriteLine("4 - Division");
-
-                //This line prompts the user to select a number between 1-4
-            Console.WriteLine("Enter your choice (1 - 4): ");
-
-                //This line takes the input of the user choice and converts it into a int to be used later
-            myChoice = Convert.ToInt32(Console.ReadLine());
-
-                //I used a switch case for this. Considering there were only 4 options I decided it'd be a good way to choose the calculation
-            switch (myChoice)
-            {
-                    //the first option, addition
-                case 1:
-                    Console.WriteLine("The result of addition is: " + userOutPut);
-
-                        //This line breaks the program to allow it to end
-                    break;
-
-                    //The second option in line
-
-                case 2:
-                    Console.WriteLine("The result of subtraction is: " + userOutPut);
-
-                        //This line breaks the code and allows it to end
-                    break;
-
-                    //the third option that can be selected (mulitplication)
-                case 3:
-                    Console.WriteLine("The result of multiplication is: " + userOutPut);
-
-                        //Breaks code and allows it to end
-                    break;
-
-                    //The fourth option, division
-                case 4:
-                    Console.WriteLine("The result of division is: " + userOutPut);
-
-                        //Breaks code and ends the program
-                    break;
-
-                    //In case the user does not enter the correct number range, it alerts them that the program only accepts the input of 1-4
-                default:
-
-                        //Alerts the user they need to enter a valid number 
-                    Console.WriteLine("Please enter a valid number between 1 and 4.");
-
-                        //Breaks program and ends it
+                switch (myChoice)
+                {
+                    case 1:
+                        userOutPut = Convert.ToDouble(firstNum + secondNum);
+                        Console.WriteLine("The result of addition is: " + userOutPut);
                         break;
+                    case 2:
+                        userOutPut = Convert.ToDouble(firstNum - secondNum);
+                        Console.WriteLine("The result of subtraction is: " + userOutPut);
+                        break;
+                    case 3:
+                        userOutPut = Convert.ToDouble(firstNum * secondNum);
+                        Console.WriteLine("The result of multiplication is: " + userOutPut);
+                        break;
+                    case 4:
+                        userOutPut = Convert.ToDouble(firstNum / secondNum);
+
+                        Console.WriteLine("The result of division is: " + userOutPut);
+                        break;
+                    case 5:
+                        Console.WriteLine("You have successfully quit!");
+                        whileTrue = false;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid number between 1 and 4.");
+                        break;
+                }
             }
             Console.ReadLine();
 
